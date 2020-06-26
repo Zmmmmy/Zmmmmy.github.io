566f7718ee23decd86820c1ee4288f5dec279917
Zmmmmy <1837860314@qq.com>
2020-06-26 19:31:59 +0800

    Site updated: 2020-06-26 19:31:57

--- js/app.js
+++ js/app.js
@@ -20,77 +20,10 @@ var appDaily = {
       this.googleSearch($ipt.value.trim());
     }
 
-    return false; header
+    return false;
   },
 };
 
-var whetherChange = 0;
-var whetherChangeToTop = 0;
-var ticking = false;
-cover = 240
+$(".toc li").replaceWith(function () { return "<li class='mdui-menu-item'>" + this.innerHTML + "</li>"; });
 
-window.onscroll = function () {
-  var howFar = document.documentElement.scrollTop || document.body.scrollTop;
-  if (howFar > cover & whetherChange == 0) {
-    $("#appbar-index").addClass("itp-appbar-color-pull-up");
-    whetherChange = 1;
-  };
-  if (howFar <= cover & whetherChange == 1) {
-    $("#appbar-index").removeClass("itp-appbar-color-pull-up");
-    whetherChange = 0;
-  }
-};
-
-/*
-
-$(".toc li").replaceWith(function () {
-  return "<li class='mdui-menu-item'>" + this.innerHTML + "</li>";
-});
-
-$(".toc-child li").replaceWith(function () {
-  return "<li class='mdui-menu-item'>" + this.innerHTML + "</li>";
-});
-
-$(".itp-post-toc ol").replaceWith(function () {
-  return "<ul class='mdui-menu mdui-menu-cascade itp-toc-menu' id='toc'>" + this.innerHTML + "</ul>";
-});
-
-*/
-
-$(".categories .archive-link23").replaceWith(function () {
-  return "<div class='mdui-chip'><span class='mdui-chip-title'>" + this.innerHTML + "</span></div>";
-});
-
-
-$("img").not('footer img,.drawer-img, .mdui-card-header-avatar,.itp-mag-content img,.itp-post-author-avatar img,.itp-post-gallery img,.nofancy').each(function () {
-  // $(this).attr("data-fancybox", "gallery"); 直接给img添加data-fancybox会导致点击图片后图片消失
-  var element = document.createElement("a");
-  $(element).attr("data-fancybox", "gallery");
-  $(element).attr("href", $(this).attr("src"));
-  $(this).wrap(element);
-})
-
-$(".itp-mag-content img").each(function () {
-  // $(this).attr("data-fancybox", "gallery"); 直接给img添加data-fancybox会导致点击图片后图片消失
-  var element = document.createElement("div");
-  $(element).attr("class", "itp-mag-img");
-  $(this).wrap(element);
-})
-
-$('[data-fancybox="gallery"]').fancybox({
-  buttons: [
-    "close",
-  ],
-  baseTpl:
-    '<div class="fancybox-container" role="dialog" tabindex="-1">' +
-    '<div class="fancybox-bg"></div>' +
-    '<div class="fancybox-inner">' +
-    '<div class="fancybox-infobar">' +
-    '<span data-fancybox-index></span>&nbsp;/&nbsp;<span data-fancybox-count></span>' +
-    '</div>' +
-    '<div class="fancybox-toolbar">{{buttons}}</div>' +
-    '<div class="fancybox-navigation">{{arrows}}</div>' +
-    '<div class="fancybox-stage"></div>' +
-    '</div>' +
-    '</div>',
-});
+$(".itp-post-toc ol").replaceWith(function () { return "<ul class='mdui-menu mdui-menu-cascade itp-toc-menu' id='toc'>" + this.innerHTML + "</ul>"; });
