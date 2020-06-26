607f684df8d4370ba29367b83d9029a44a4baa52
Zmmmmy <1837860314@qq.com>
2020-06-26 19:28:02 +0800

    Site updated: 2020-06-26 19:28:01

--- js/search.js
+++ js/search.js
@@ -1,85 +1,76 @@
-var searchFunc = function (path, search_id, content_id) {
-    'use strict';
-    $.ajax({
-        url: path,
-        dataType: "xml",
-        success: function (xmlResponse) {
-            // get the contents from search data
-            var datas = $("entry", xmlResponse).map(function () {
-                return {
-                    title: $("title", this).text(),
-                    content: $("content", this).text(),
-                    url: $("url", this).text()
-                };
-            }).get();
-            var $input = document.getElementById(search_id);
-            var $resultContent = document.getElementById(content_id);
-            $input.addEventListener('input', function () {
-                var str = '<ul class=\"search-result-list\">';
-                var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
-                $resultContent.innerHTML = "";
-                if (this.value.trim().length <= 0) {
-                    return;
-                }
-                // perform local searching
-                datas.forEach(function (data) {
-                    var isMatch = true;
-                    var content_index = [];
-                    var data_title = data.title.trim().toLowerCase();
-                    var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
-                    var data_url = data.url;
-                    var index_title = -1;
-                    var index_content = -1;
-                    var first_occur = -1;
-                    // only match artiles with not empty titles and contents
-                    if (data_title != '' && data_content != '') {
-                        keywords.forEach(function (keyword, i) {
-                            index_title = data_title.indexOf(keyword);
-                            index_content = data_content.indexOf(keyword);
-                            if (index_title < 0 && index_content < 0) {
-                                isMatch = false;
-                            } else {
-                                if (index_content < 0) {
-                                    index_content = 0;
-                                }
-                                if (i == 0) {
-                                    first_occur = index_content;
-                                }
-                            }
-                        });
-                    }
-                    // show search results
-                    if (isMatch) {
-                        str += "<li><a href='" + data_url + "' class='search-result-title'>" + data_title + "</a>";
-                        var content = data.content.trim().replace(/<[^>]+>/g, "");
-                        if (first_occur >= 0) {
-                            // cut out 100 characters
-                            var start = first_occur - 20;
-                            var end = first_occur + 80;
-                            if (start < 0) {
-                                start = 0;
-                            }
-                            if (start == 0) {
-                                end = 100;
-                            }
-                            if (end > content.length) {
-                                end = content.length;
-                            }
-                            var match_content = content.substr(start, end);
-                            // highlight all keywords
-                            keywords.forEach(function (keyword) {
-                                var regS = new RegExp(keyword, "gi");
-                                match_content = match_content.replace(regS, "<em class=\"search-keyword\">" + keyword + "</em>");
-                            });
+$(document).ready(function() {
+  Array.prototype.forEach.call(document.querySelectorAll('.mdl-card__media'), function(el) {
+      var link = el.querySelector('a');
+      if (!link) {
+          return;
+      }
+      var target = link.getAttribute('href');
+      if (!target) {
+          return;
+      }
+      el.addEventListener('click', function() {
+          location.href = target;
+      });
+  });
 
-                            str += "<p class=\"search-result\">" + match_content + "...</p>"
-                        }
-                        str += "</li>";
-                    }
-                });
-                str += "</ul>";
-                $resultContent.innerHTML = str;
-            });
-        }
-    });
-}
+  // Auto fill visitor-url "http://"
+  $("#visitor-url").focus(function() {
+      this.placeholder = "http://";
+  });
+  $("#visitor-url").blur(function() {
+      this.placeholder = "";
+  });
+
+  // Auto delete input content & placeholder
+  $(".search-input").blur(function() {
+      this.value = "";
+      this.placeholder="";
+  });
+
+  // Auto hidden share/tags popup block
+  $('#article-fuctions-share-button, #article-functions-viewtags-button').click(function() {
+      $('.is-visible').removeClass('is-visible');
+  });
+
+  // Add 'fab' class to the PageNav <a>
+  $('.fabs .prev, .fabs .next, .fabs .prev-content').addClass('fab');
+
+  // Smooth scroll to Top
+  $("a.toTop").click(function() {
+      $("html, body").animate({
+          scrollTop: $($(this).attr("href")).offset().top + "px"
+      }, {
+          duration: 500,
+          easing: "swing"
+      });
+      return false;
+  });
+
+
+  // Smooth scroll of TOC
+  $(".post-toc a").click(function() {
+      $("html, body").animate({
+          scrollTop: $($(this).attr("href")).offset().top + "px"
+      }, {
+          duration: 500,
+          easing: "swing"
+      });
+      return false;
+  });
+
+  // Click anywhere to close the FAB menu
+  $(document).click(function() {
+      if( $("#prime").hasClass("is-visible") ) {
+          $("#prime").click();
+      }
+  });
+  $("#prime").click(function(e) {
+      e.stopPropagation();
+      return false;
+  });
+
+  // Click anywhere to remove local search result
+  $(document).click(function() {
+      $("#local-search-result ul").css("display", "none");
+  });
+});
